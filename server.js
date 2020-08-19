b0446de9eaee127235c7cf0a4addc32c900e58c1
DinethP <dineth_perera@hotmail.com>
2020-08-19 09:52:21 +0530

    Added EJS

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app)
 
+// setup ejs (embedded javascript)
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-  res.status(200).send("Hello World");
+  // res.status(200).send("Hello World");
+  res.render('room')
 });
 
 
