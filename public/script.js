fafe7845ade1d153fb26377ec717db26dd80ee2c
DinethP <dineth_perera@hotmail.com>
2020-08-21 10:58:04 +0530

    setting up peer.js

--- public/script.js
+++ public/script.js
@@ -1,13 +1,19 @@
 // Javascript for frontEnd
 
-const { connect } = require("http2");
-
 // Importing socket
 const socket = io('/');
 
 // Create video HTML Tag
 const myVideo = document.createElement('video');
 myVideo.muted = true;
+
+// create peer
+var peer = new Peer(undefined, {
+  // path to our server
+  path: '/peerjs',
+  host: '/',
+  port: '3000'
+}); 
 
 // get the video-grid html div element
 const videoGrid = document.getElementById('video-grid');
@@ -17,14 +23,29 @@ let myVideoStream;
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
-}).then(stream => {
+}).then((stream) => {
   myVideoStream = stream;
   addVideoStream(myVideo, stream);
 })
 
-// Send event to our server
-// ROOM_ID is passed in from room.ejs
-socket.emit('join-room', ROOM_ID);
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener('loadedmetadata', () => {
+    video.play();
+  });
+  // Append the video to the video-grid div in room.ejs
+  videoGrid.append(video);
+}
+
+// this id is unique for everyone who is connecting
+peer.on('open', (id) => {
+  // Send event to our server
+  // ROOM_ID is passed in from room.ejs
+  socket.emit('join-room', ROOM_ID, id);
+})
+
+
+
 
 // This will be listened to by server.js
 socket.on('user-connected', () => {
@@ -35,11 +56,3 @@ const connectToNewUser = () => {
   console.log('new user');
 }
 
-const addVideoStream = (video, stream) => {
-  video.srcObject = stream;
-  video.addEventListener('loadedmetadata', () => {
-    video.play();
-  });
-  // Append the video to the video-grid div in room.ejs
-  videoGrid.append(video);
-}
--- server.js
+++ server.js
@@ -8,14 +8,17 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 // import uuid version 4
 const { v4: uuidv4} = require('uuid');
-const { resolveSoa } = require('dns');
-const { connect } = require('http2');
+const { ExpressPeerServer } = require('peer');
+// Using PeerServer together with express
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
 // setup ejs (embedded javascript)
 app.set('view engine', 'ejs');
 // set the public folder (Very important)
 app.use(express.static('public'));
 
-
+app.use('/peerjs', peerServer);
 app.get('/', (req, res) => {
   // res.status(200).send("Hello World");
   // Redirect the user to the unique zoom room
@@ -32,16 +35,16 @@ app.get('/:room', (req, res) => {
 // make a socket.io connection
 // runs when someone connects to the webpage
 // 'socket' is the socket that a user connects to
-io.on('connection', socket => {
+io.on('connection', (socket) => {
   // events to listen to from frontend
 
   // listen to 'join-room' emitted from script.js
-  socket.on('join-room', () => {
+  socket.on('join-room', (roomId) => {
     // make the socket(user) join a room
     socket.join(roomId);
     // send a message to the room you're currently
     // this message is sent to everyone else in the room except the user
-    // who is newly
+    // who is newly connected
     // 'user-connected' is the event
     socket.to(roomId).broadcast.emit('user-connected');
   })
